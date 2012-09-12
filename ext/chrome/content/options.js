0646c6a5e00044bc79720219531cb9d975edec07
kcome <harryl.byread@gmail.com>
2012-09-12 23:40:25 +0800

    use firefox-profile or not is handled :)

--- ext/chrome/content/options.js
+++ ext/chrome/content/options.js
@@ -6,7 +6,15 @@ t20brs.options = {
 
   updateform: function() {
     var disabled = !document.getElementById("enable").checked;
+    var useff = document.getElementById("useffprofile").checked;
+    var file = FileUtils.getFile("AChrom", [".use-firefox-profile"]);
+    if (useff) {
+      if (!file.exists())
+        file.create(0, 384);
+    } else {
+      if (file.exists())
+        file.remove(false);
+    }
   }
-    
 };
 
